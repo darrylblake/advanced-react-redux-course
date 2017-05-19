f5703b495e144860528a132571afd27a304cd7f0
Darryl Blake <darryl@darrylblake.com>
2017-05-19 10:48:19 +0700

    Add middleware for logging with `morgan` and parse all incoming requests as JSON

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const http = require('http'); // Native Node library, for low level HTTP requests
+const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
-// App Setup
-app.use(morgan('combined'));
+app.use(morgan('combined')); // Morgan is a logging framework
 app.use(bodyParser.json({ type: '*/*' }));
 
-// Server Setup
 const port = process.env.PORT || 3090;
-const server = http.createServer(app); // Forward HTTP requests to express application
+const server = http.createServer(app);
 server.listen(port);
 
 console.log(`Server listening on ${port}`);
