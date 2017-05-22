51d4c68b7b518df206174aaf603d16dd78044ac8
Darryl Blake <darryl@darrylblake.com>
2017-05-22 10:35:31 +0700

    Generates a JWT token

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = (req, res, next) => {
   const { email, password } = req.body;
@@ -22,7 +29,7 @@ exports.signup = (req, res, next) => {
     user.save((err) => {
       if (err) return next(err);
 
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
