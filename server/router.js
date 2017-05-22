388ddc238aea444b52d4f99521aafd58d5c20acc
Darryl Blake <darryl@darrylblake.com>
2017-05-22 12:59:18 +0700

    Checks correct JWT token has been provided

--- server/router.js
+++ server/router.js
@@ -1,5 +1,13 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// { session: false } to prevent Passport using cookies
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send('Authorized!');
+  })
   app.post('/signup', Authentication.signup);
 }
