bd1df85ce4beef01816c2d6f33814a2272d611c1
Darryl Blake <darryl@darrylblake.com>
2017-05-23 10:39:56 +0700

    Enables CORS on the server

--- server/index.js
+++ server/index.js
@@ -5,10 +5,12 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost:auth/dbname');
 
 app.use(morgan('combined')); // Morgan is a logging framework
+app.use(cors()); // https://github.com/expressjs/cors
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
