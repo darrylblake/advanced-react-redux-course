56508f5773ed8db149281e6fb48f9a5e88af2f5b
Darryl Blake <darryl@darrylblake.com>
2017-05-23 11:12:43 +0700

    Redirects to `/feature` on successfully login

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+    .then(resonse => {
+      browserHistory.push('/feature');
+    })
+    .catch(() => {
+
+    });
   }  
 };
