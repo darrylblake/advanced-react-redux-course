79f47b9503362498ddf4e27170ae28f14c1b80b0
Darryl Blake <darryl@darrylblake.com>
2017-05-23 14:51:31 +0700

    Update links depending on wether user is authenticated

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { authenticated } = this.props;
+
     return (
       <nav className="navbar navbar-light">
+        <Link className="navbar-brand" to="/">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            <Link to="/signin">Sign in</Link>
-          </li>
+          {!authenticated && [
+            <li className="nav-item" key="1">
+              <Link className="nav-link" to="/signin">Sign in</Link>
+            </li>,
+            <li className="nav-item" key="2">
+              <Link className="nav-link" to="/signup">Sign up</Link>
+            </li>
+          ]}
+          {authenticated &&
+            <li className="nav-item">
+              <Link className="nav-link" to="/signout">Sign out</Link>
+            </li>
+          }
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth: { authenticated } }) => ({
+  authenticated,
+});
+
+export default connect(mapStateToProps)(Header);
